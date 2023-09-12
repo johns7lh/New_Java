d8c0d46ef870a9e496fa0f9913a8c52ef424975b
johns7lh <jlauren1111@yahoo.com>
2023-09-12 14:56:04 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 16;
+        String greeting= "Hello Joe mama!";
+
+        System.out.println(greeting);
+        System.out.println(greeting + " rawr");
+    }
+}
